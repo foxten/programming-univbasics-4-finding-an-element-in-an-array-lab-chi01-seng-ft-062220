77407d10632796a2eb8e48a0091a0d88f30d8a3f
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-22 21:56:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
   i = 0 
-  while i< array.length do
-    if array.include?(value_to_find) == true
-      return i
-    end
+  while array[i] do
+  if array[i] == value_to_find
+    return i
+  end
     i+=1
   end
 end
