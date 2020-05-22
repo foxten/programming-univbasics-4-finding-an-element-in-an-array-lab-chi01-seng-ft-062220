428046fc6901838c3cced7a97f331c5d025577fe
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-22 21:55:24 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   i = 0 
   while i< array.length do
     if array.include?(value_to_find) == true
-      return array[i]
+      return i
     end
     i+=1
   end
